c14f72ea0e254806add20ce0fad6a65d442a9c78
shobhak6264 <47355074+shobhak6264@users.noreply.github.com>
2022-12-07 22:27:13 -0500

    Updated Changes
    1.Added color pallete to the graphs

--- src/components/charts.js
+++ src/components/charts.js
@@ -58,6 +58,8 @@ export const LineChart = () => {
                     type: checkGraphType(graphData),
                     panning: true
                 },
+                colors: ['#DB843D', '#AA4643', '#89A54E', '#80699B', '#3D96AE',
+        '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92'],
                   title: {text : "Question"+"  "+(index+1)},
                   xAxis: {
                     categories: categories,
